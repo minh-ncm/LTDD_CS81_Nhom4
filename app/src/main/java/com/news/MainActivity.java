f43a9e47db1d1d2b49c62bf190aa433ece9cfa97
sie <astro.toxic1203@gmail.com>
2021-03-28 22:48:53 +0700

    change real-time database to firestore api

--- app/src/main/java/com/news/MainActivity.java
+++ app/src/main/java/com/news/MainActivity.java
@@ -1,16 +1,27 @@
 package com.news;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     Button btnOne, btnTwo;
+    TextView txtHello;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,20 +29,32 @@ public class MainActivity extends AppCompatActivity {
 
         btnOne = (Button) findViewById(R.id.btnOne);
         btnTwo = (Button) findViewById(R.id.btnTwo);
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
+        txtHello = (TextView) findViewById(R.id.textView);
+
+        Map<String, Object> docData = new HashMap<>();
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference collRef = db.document("users/testId");
 
         btnOne.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                myRef.setValue("One");
+                docData.put("testId", "one");
+                docData.put("testField", 1);
+                db.collection("users").document("testId").set(docData);
             }
         });
 
         btnTwo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                myRef.setValue("Two");
+                collRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        DocumentSnapshot document = task.getResult();
+                        txtHello.setText(document.getData().toString());
+                    }
+                });
             }
         });
     }
