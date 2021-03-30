4788730f9f7f7ff63e94438a692063175df4cb63
sie <astro.toxic1203@gmail.com>
2021-03-30 11:29:52 +0700

    move backend processes to new activity and layout

---
+++ app/src/main/java/com/news/BackendTestActivity.java
@@ -0,0 +1,56 @@
+        package com.news;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.backend.New;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class BackendTestActivity extends AppCompatActivity {
+    Button btnOne, btnTwo;
+    TextView txtHello;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_backend_test);
+
+        btnOne = (Button) findViewById(R.id.btnOne);
+        btnTwo = (Button) findViewById(R.id.btnTwo);
+        txtHello = (TextView) findViewById(R.id.textView);
+        txtHello.setMovementMethod(new ScrollingMovementMethod());
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference docRef = db.document("news/firstNew");
+        String content = New.createSampleContent(4, 5);
+        New paper = new New("test", content, "");
+
+        btnOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                docRef.set(paper);
+                txtHello.setText(paper.getContent());
+            }
+        });
+
+        btnTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        New n = documentSnapshot.toObject(New.class);
+                        txtHello.setText(n.getTitle());
+                    }
+                });
+            }
+        });
+    }
+}
