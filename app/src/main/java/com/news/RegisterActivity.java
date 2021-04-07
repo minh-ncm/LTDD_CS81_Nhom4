e6ad54074ef98528edb9849399cc054ae99f5d41
LamNguyen <nguyenhuuduylinh23@gmail.com>
2021-04-07 14:24:25 +0700

    add register-fix login

--- app/src/main/java/com/news/RegisterActivity.java
+++ app/src/main/java/com/news/RegisterActivity.java
@@ -1,23 +1,22 @@
 package com.news;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+public class RegisterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        TextView btn = findViewById(R.id.textViewSignUp);
+        setContentView(R.layout.activity_register);
+        TextView btn = findViewById(R.id.alreadyHaveAccount);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
+                startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
             }
         });
     }
